d1918513041312d95c7933b2bafc2cb1cf903d38
Laur Sisask <laur.sisask@gmail.com>
2022-01-23 10:34:59 +0200

    Revert "Perform optimizer step after every backpropagation step"
    This reverts commit f04a2eef995fac19ac8019101c39c0b474700db5.

--- duels-training-python/duels_training/ppo.py
+++ duels-training-python/duels_training/ppo.py
@@ -1,5 +1,6 @@
 import copy
 import logging
+import math
 import os
 import threading
 import time
@@ -178,16 +179,18 @@ def standardize(x, mask):
 
 def train_on_batch(device, optimizer, model, model_old, policy, batch, backpropagation_steps,
                    clip_range, max_grad_norm, value_coefficient, entropy_coefficient):
+    model.zero_grad()
+
     all_observations, all_actions, all_returns, all_advantages, all_mask = batch
 
     all_advantages = standardize(all_advantages, all_mask)
+
+    backward_steps = math.ceil(all_observations.size(1) / backpropagation_steps)
 
     actor_loss_sum = 0
     critic_loss_sum = 0
     entropy_loss_sum = 0
     loss_sum = 0
-    grad_norm_sum = 0
-    num_microbatches = 0
 
     last_state = None
     last_state_old = None
@@ -199,8 +202,6 @@ def train_on_batch(device, optimizer, model, model_old, policy, batch, backpropa
             all_advantages.split(backpropagation_steps, dim=1),
             all_mask.split(backpropagation_steps, dim=1)
     ):
-        model.zero_grad()
-
         observations = observations.to(device)
         actions = actions.to(device)
         returns = returns.to(device)
@@ -256,29 +257,31 @@ def train_on_batch(device, optimizer, model, model_old, policy, batch, backpropa
         assert entropy_loss.numel() == 1
 
         loss = actor_loss + value_coefficient * critic_loss + entropy_coefficient * entropy_loss
-        loss.backward()
-
-        assert loss < 1e6
-
-        grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
-        optimizer.step()
+        (loss / backward_steps).backward()
 
         actor_loss_sum += actor_loss.item()
         critic_loss_sum += critic_loss.item()
         entropy_loss_sum += entropy_loss.item()
         loss_sum += loss.item()
-        grad_norm_sum += grad_norm.item()
-        num_microbatches += 1
+
+        assert loss < 1e6
 
         last_state.detach_()
 
+    grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+    optimizer.step()
+
+    avg_loss = torch.tensor([loss_sum / backward_steps], dtype=torch.float32, device=device).item()
+    avg_actor_loss = torch.tensor([actor_loss_sum / backward_steps], dtype=torch.float32, device=device).item()
+    avg_critic_loss = torch.tensor([critic_loss_sum / backward_steps], dtype=torch.float32, device=device).item()
+    avg_entropy_loss = torch.tensor([entropy_loss_sum / backward_steps], dtype=torch.float32, device=device).item()
+
     return (
-        loss_sum,
-        actor_loss_sum,
-        critic_loss_sum,
-        entropy_loss_sum,
-        grad_norm_sum,
-        num_microbatches
+        avg_loss,
+        avg_actor_loss,
+        avg_critic_loss,
+        avg_entropy_loss,
+        grad_norm.item()
     )
 
 
@@ -325,7 +328,7 @@ def train(device, dataset, model, policy, optimizer, metrics, start_iteration, n
         num_of_batches = 0
 
         for batch in data_loader:
-            loss, actor_loss, critic_loss, entropy_loss, grad_norm, num_microbatches = train_on_batch(
+            loss, actor_loss, critic_loss, entropy_loss, grad_norm = train_on_batch(
                 device,
                 optimizer,
                 model,
@@ -339,7 +342,7 @@ def train(device, dataset, model, policy, optimizer, metrics, start_iteration, n
                 entropy_coefficient=entropy_coefficient
             )
 
-            num_of_batches += num_microbatches
+            num_of_batches += 1
 
             loss_sum += loss
             actor_loss_sum += actor_loss
@@ -349,7 +352,7 @@ def train(device, dataset, model, policy, optimizer, metrics, start_iteration, n
             metrics.add_scalar("Loss/Actor", actor_loss_sum / num_of_batches, iteration)
             metrics.add_scalar("Loss/Critic", critic_loss_sum / num_of_batches, iteration)
             metrics.add_scalar("Loss/Entropy", entropy_loss_sum / num_of_batches, iteration)
-            metrics.add_scalar("Unclipped gradient norm", grad_norm / num_of_batches, iteration)
+            metrics.add_scalar("Unclipped gradient norm", grad_norm, iteration)
 
             iteration += 1
 
