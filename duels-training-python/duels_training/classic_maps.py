af46281abd9289896fd898422bfdd4e6f9dd8919
Laur Sisask <laur.sisask@gmail.com>
2022-04-07 07:13:19 +0300

    Remove duplicate from classic duels maps

--- duels-training-python/duels_training/classic_maps.py
+++ duels-training-python/duels_training/classic_maps.py
@@ -1,1 +1,1 @@
-MAPS = ["arena", "arena", "fractal", "reef", "fight_night", "highset", "museum", "skyport", "spikerock_bay", "backwoods"]
+MAPS = ["arena", "fractal", "reef", "fight_night", "highset", "museum", "skyport", "spikerock_bay", "backwoods"]
